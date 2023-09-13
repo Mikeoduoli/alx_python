be1eb042eb171850db40fbd5511029e4347b6ec1
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-13 06:00:51 +0300

    Postive or Negative

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,5 @@
 import random
-number = random.randint(-10000, 10000)
+number = random.randit(-10000, 10000)
 last_digit = abs(number) % 10
 print(f"Last digit of {number} is {last_digit}", end=' ')
 if last_digit > 5:
