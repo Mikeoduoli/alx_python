aed795f308aafe4a270cb7d7b673ab5e58b5a566
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-13 14:28:36 +0300

    Python - Functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -7,7 +7,7 @@ def fibonacci_sequence(n):
 
     fibonacci_list = [0, 1]
 
-    for i in range(5):
+    for i in range(4):
         next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
         fibonacci_list.append(next_fibonacci)
 
