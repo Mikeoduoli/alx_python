98798be7c5af82a1943813d48d910917f5f832fa
Mikeoduoli <mikeoduoli@gmail.com>
2023-09-14 05:54:39 +0300

    Data Structures: Lists, Tuples Python

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""  # Initialize an empty string to store the modified result
+
+    for char in my_string:
+        # Check if the character is a lowercase or uppercase 'c'
+        if char not in ['c', 'C']:
+            new_string += char  # Add the character to the modified string
+
+    return new_string  # Return the modified string
